b436d3fd12f868b8551d288347abdb0969a7d43a
Elzy🐧 <elzyproot@protonmail.com>
2026-02-28 08:53:32 +0700

    Refactor import alias for config_ui to ui

--- lib/core/commands/help.py
+++ lib/core/commands/help.py
@@ -4,11 +4,11 @@
 # See LICENSE file in the project root for full license information.
 
 
-import app.base.config_ui as config_ui
+import app.base.config_ui as ui
 
 
 # Display help to make it easier for users to see what commands are available.
 # Without this the user is confused about what commands are in storm.
 def execute(args, context):
-    config_ui.show_help()
+    ui.show_help()
     return context
