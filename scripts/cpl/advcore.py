23c01ee1c51f9e03e218b8c10ede17f447b2affa
Elzy🐧 <elzyproot@protonmail.com>
2026-02-28 09:57:19 +0700

    Update print statements for safe mode detection

--- scripts/cpl/advcore.py
+++ scripts/cpl/advcore.py
@@ -1,17 +1,17 @@
 # MIT License.
 # Copyright (c) 2026 Storm Framework
-
 # See LICENSE file in the project root for full license information.
-
-
 import os
+
 def safe_mode():
     is_android = "TERMUX_VERSION" in os.environ or os.path.exists("/data/data/com.termux")
     total_cores = os.cpu_count() or 1
+    
     if is_android:
         workers = max(1, total_cores - 2)
-        print(f"[*] Linux detected. Using safe mode: {workers}/{total_cores} cores")
+        print(f"[*] Linux detected > {workers}/{total_cores} cores")
     else:
         workers = total_cores
-        print(f"[*] Linux Standar detected. Unleashing full power: {workers} cores")
+        print(f"[*] Linux Standar detected > {workers} cores")
+        
     return workers
