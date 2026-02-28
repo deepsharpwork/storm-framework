6dfd355fd5bd2e3bbb6c1b35e751a9be19acb4fe
Elzy🐧 <elzyproot@protonmail.com>
2026-02-28 09:50:57 +0700

    Update function docstring and comments for clarity

--- lib/core/handler.py
+++ lib/core/handler.py
@@ -10,18 +10,18 @@ from app.utility.colors import C
 
 
 def execute(cmd, args, context):
-    """Fungsi pusat untuk mencari dan menjalankan file perintah."""
-    # Format path: lib/core/commands/use.py
+    """Central function to search for and execute command files."""
+    
     cmd_path = os.path.join("lib", "core", "commands", f"{cmd}.py")
     if os.path.exists(cmd_path):
         try:
-            # Import dinamis sesuai perintah
+            # Dynamic import on command
             module = importlib.import_module(f"lib.core.commands.{cmd}")
-            # Panggil fungsi utama di dalam file tersebut
+            
+            # call dynamic function
             return module.execute(args, context)
         except Exception as e:
-            print(f"{C.ERROR}[-] Error in command '{cmd}': {e}")
+            print(f"{C.ERROR}[-] ERROR => {cmd} > {e}")
             return context
 
-    # Jika file tidak ditemukan di folder commands
     return None
