edaf5a2f6a82b43d7c26e4b2acbc366ae0da0f10
Elzy🐧 <elzyproot@protonmail.com>
2026-03-01 18:42:22 +0700

    Update ghunt.py

--- modules/auxiliary/osint/email/ghunt.py
+++ modules/auxiliary/osint/email/ghunt.py
@@ -23,24 +23,31 @@ def execute(options):
     output_dir = Path(__file__).parent / "results"
     output_dir.mkdir(parents=True, exist_ok=True)
     json_out_path = output_dir / output_filename
+    
+    check_ghunt = os.path.join(ROOT, "script", "ghunt", ".git")
+    if os.path.exists(check_ghunt)
+        payload = {
+            "module": module,
+            "target": target,
+            "json_out": str(json_out_path),
+        }
+        try:
+            process = subprocess.run(
+                [str(python_executable), str(worker_script)],
+                input=json.dumps(payload),  # Kirim JSON ke stdin
+                text=True,
+                capture_output=True,
+                check=True,
+            )
+            return {"status": "success", "output": process.stdout}
+        except KeyboardInterrupt:
+            pass
+        except subprocess.CalledProcessError as e:
+            return {"status": "error", "message": e.stderr}
+        except Exception as e:
+            return {"status": "error", "message": str(e)} 
+    else:
+        print("[-] ")
 
-    payload = {
-        "module": module,
-        "target": target,
-        "json_out": str(json_out_path),
-    }
-    try:
-        process = subprocess.run(
-            [str(python_executable), str(worker_script)],
-            input=json.dumps(payload),  # Kirim JSON ke stdin
-            text=True,
-            capture_output=True,
-            check=True,
-        )
-        return {"status": "success", "output": process.stdout}
-    except KeyboardInterrupt:
-        pass
-    except subprocess.CalledProcessError as e:
-        return {"status": "error", "message": e.stderr}
-    except Exception as e:
-        return {"status": "error", "message": str(e)}
+    
+    
