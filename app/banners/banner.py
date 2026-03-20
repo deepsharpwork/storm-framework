aa825c63ed007e75167d666842aa1ac2af4cb871
エルジー🐧 <elzyproot@protonmail.com>
2026-03-20 12:53:15 +0700

    Increase padding for banner lines

--- app/banners/banner.py
+++ app/banners/banner.py
@@ -31,7 +31,7 @@ def get_random_banner():
         if not lines:
             return raw_banner
 
-        padding_str = " " * 0
+        padding_str = " " * 5
 
         result = "\n".join([f"{padding_str}{line}" for line in lines])
 
