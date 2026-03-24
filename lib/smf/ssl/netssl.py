a99afd8183aba64ab631b5040dc493daf5d8cdf7
Proot9 <elzyproot@protonmail.com>
2026-03-24 13:02:26 +0700

    add: connections ssl encrypt

---
+++ lib/smf/ssl/netssl.py
@@ -0,0 +1,16 @@
+import requests
+import urllib3
+import ssl
+
+def storm_ssl(method, url, **kwargs):
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    try:
+        kwargs.setdefault('verify', False)
+        kwargs.setdefault('timeout', 15)
+
+        return requests.request(method, url, **kwargs)
+
+    except requests.exceptions.SSLError as e:
+        return f"SSL_HANDSHAKE_FAILED: {e}"
+    except Exception as e:
+        return f"CONNECTION_ERROR: {e}"
