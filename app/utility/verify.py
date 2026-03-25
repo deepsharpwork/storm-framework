269768ae4535035ef52f900627eb968b60821a0e
エルジー🐧 <elzyproot@protonmail.com>
2026-03-26 01:42:40 +0700

    Refactor error messages and variable names in verify.py

--- app/utility/verify.py
+++ app/utility/verify.py
@@ -5,16 +5,16 @@ from app.utility.colors import C
 
 
 def run_verif():
-    bin_p = "external/source/binary/check"
+    lib = "external/source/binary/check"
     if not os.path.exists(bin_p):
-        print(f"[-] ERROR: Rust binary not found in {bin_p}")
+        print(f"[-] ERROR => Rust binary not found in {lib}")
         sys.exit(1)
-    print(f"[*] [INTEGRITY] Launching Storm Engine")
+    print(f"[∆] [INTEGRITY STORM RUNNING] [∆]")
     try:
-        result = subprocess.run([bin_p])
+        result = subprocess.run([lib])
 
         if result.returncode != 0:
-            print(f"\n[-] CRITICAL: Reinstall Storm for security.)")
+            print(f"\n[-] CRITICAL => Reinstall Storm for security.)")
             sys.exit(result.returncode)
 
         return True
@@ -22,13 +22,13 @@ def run_verif():
     except KeyboardInterrupt:
         return
     except Exception as e:
-        print(f"[-] ERROR during execution: {e}")
+        print(f"[-] ERROR => {e}")
         sys.exit(1)
 
 
 def check_critical_files():
     if not os.path.exists(".env"):
-        print(f"{C.ERROR}[!] CRITICAL: Integrity Key (.env) is missing!{C.RESET}")
+        print(f"{C.ERROR}[!] CRITICAL => Integrity Key (.env) is missing!{C.RESET}")
         print(
             f"[*] Storm cannot verify the database signature without your unique keys."
         )
