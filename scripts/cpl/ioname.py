ee81c3a69fa7103a7ec9265059d5a36c4030bfc8
エルジー🐧 <elzyproot@protonmail.com>
2026-03-27 04:25:23 +0700

    Enhance get_bin_name for cdylib support
    Refactor get_bin_name to handle cdylib cases and update regex.

--- scripts/cpl/ioname.py
+++ scripts/cpl/ioname.py
@@ -1,19 +1,18 @@
 import re
 import os
 
-
 def get_bin_name(path):
     with open(path, "r") as f:
         txt = f.read()
-        # Search for name in block [[bin]]
-        res = re.findall(r'\[(?:\[bin\]|package)\].*?name\s*=\s*"([^"]+)"', txt, re.S)
-        return (
-            res[-1].replace("-", "_")
-            if res
-            else os.path.basename(os.path.dirname(path))
-        )
-
-
+        is_cdylib = "cdylib" in txt
+        res = re.findall(r'\[(?:\[bin\]|package|lib)\].*?name\s*=\s*"([^"]+)"', txt, re.S)
+        if res:
+            name = res[-1].replace("-", "_")
+            if is_cdylib:
+                return f"lib{name}.so"
+            return name
+        return os.path.basename(os.path.dirname(path))
+    
 # Looking up I/O names inside Cargo.toml in each Rust function
 # [[bin]] always needs to be paid attention to in order to compile stably
 # It is important to note that Cargo.toml must always be present alongside Rust code files without src.
