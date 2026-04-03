b688c370b705424a3d84b838746f30f266a4f92c
エルジー🐧 <elzyproot@protonmail.com>
2026-04-03 20:44:39 +0700

    Update exit.py

--- lib/core/commands/exit.py
+++ lib/core/commands/exit.py
@@ -1,3 +1,6 @@
+# -- https://github.com/StormWorld0/storm-framework 
+# -- SMF License 
+# # #
 # Exit command to avoid errors or crashes in storm.
 # Because if you only use CTRL + C it is possible that the storm will come out messy.
 # This will minimize the possibility of a crash to prevent damage.
